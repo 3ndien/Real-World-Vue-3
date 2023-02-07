d22be3ab5187880c535ac1c016c7c092583892da
Nikolay Nikolov <3ndien@gmail.com>
2023-02-07 18:43:35 +0200

    added: nestet routes and layout for event details, reactive gstore for confirm message for register button

--- src/router/index.js
+++ src/router/index.js
@@ -1,25 +1,64 @@
 import { createRouter, createWebHistory } from "vue-router";
 import EventList from "../views/EventList.vue";
-import EventDetails from "../views/EventDetails.vue";
-import AboutView from "../views/AboutView";
+import EventLayout from "../views/event/Layout.vue";
+import EventDetails from "../views/event/Details.vue";
+import EventRegister from "../views/event/Register.vue";
+import EventEdit from "../views/event/Edit.vue";
+import AboutView from "../views/AboutView.vue";
+import NotFound from "../views/NotFound.vue";
+import NetworkError from "../views/NetworkError.vue";
 
 const routes = [
   {
     path: "/",
     name: "EventList",
     component: EventList,
+    props: route => ({ page: parseInt(route.query.page) || 1 })
   },
   {
     path: "/event/:id",
-    name: "EventDetails",
+    name: "EventLayout",
     props: true,
-    component: EventDetails,
+    component: EventLayout,
+    children: [
+      {
+        path: "",
+        name: "EventDetails",
+        component: EventDetails,
+      },
+      {
+        path: "register",
+        name: "EventRegister",
+        component: EventRegister,
+      },
+      {
+        path: "edit",
+        name: "EventEdit",
+        component: EventEdit,
+      },
+    ]
   },
   {
     path: "/about",
     name: "About",
     component: AboutView,
   },
+  {
+    path: "/:catchAll(.*)",
+    name: "NotFound",
+    component: NotFound
+  },
+  {
+    path: "/404/:resource",
+    name: "404Resource",
+    component: NotFound,
+    props: true
+  },
+  {
+    path: "/network-error",
+    name: "NetworkError",
+    component: NetworkError
+  }
 ];
 
 const router = createRouter({
--- src/views/EventList.vue
+++ src/views/EventList.vue
@@ -2,6 +2,22 @@
   <h1>Events For Good</h1>
   <div class="events">
     <EventCard v-for="event in events" :key="event.id" :event="event" />
+    <div class="pagination">
+      <router-link id="page-prev" :to="{ name: 'EventList', query: { page: page - 1 } }" 
+                  rel="prev"
+                  v-if="page != 1">
+                  &#60; Previus
+    </router-link>
+    <router-link id="curr" v-for="curr in allPages" :key="curr" :to="{ name: 'EventList', query: { page: curr } }"
+                  rel="current">
+                  {{ curr }}
+    </router-link>
+    <router-link id="page-next" :to="{ name: 'EventList', query: { page: page + 1 } }" 
+                  rel="next"
+                  v-if="hasNextPage">
+                  Next &#62;
+    </router-link>
+    </div>
   </div>
 </template>
 
@@ -9,25 +25,43 @@
 // @ is an alias to /src
 import EventCard from "@/components/EventCard.vue";
 import EventService from "./../services/EventService";
+import { watchEffect } from "vue";
 
 export default {
   name: "EventList",
+  props: ['page'],
   components: {
     EventCard,
   },
   data() {
     return {
       events: null,
+      totalEvents: 0,
     };
   },
   created() {
-    EventService.getEvents()
-    .then(response => {
-      this.events = response.data
+    watchEffect(() => {
+      this.events = null;
+      EventService.getEvents(2, this.page)
+      .then(response => {
+        this.events = response.data
+        this.totalEvents = response.headers['x-total-count']
+      })
+      .catch(() => {
+        this.$router.push({ name: 'NetworkError' });
+      })
     })
-    .catch(error => {
-      console.log(error)
-    })
+  },
+  computed: {
+    hasNextPage() {
+      var totalPages = Math.ceil(this.totalEvents / 2)
+
+      return this.page < totalPages;
+    },
+
+    allPages() {
+      return Math.ceil(this.totalEvents / 2);
+    }
   }
 };
 </script>
@@ -37,5 +71,29 @@ export default {
   display: flex;
   flex-direction: column;
   align-items: center;
+}
+
+.pagination {
+  display: flex;
+  width: 290px;
+}
+
+.pagination a{
+  flex: 1;
+  text-decoration: none;
+  color: #2c3e50;
+}
+
+#page-prev {
+  text-align: left;
+}
+
+#curr {
+  text-align: center;
+  flex: 0.3; 
+}
+
+#page-next {
+  text-align: right;
 }
 </style>
